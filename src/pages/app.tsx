36a7b0034555dee44f8701b67ad4d424487d0b92
林岑影 <lincenying@qq.com>
2023-09-26 16:23:06 +0800

    chore: 提升antd的样式,以免被reset样式覆盖

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { CSSTransition, TransitionGroup } from 'react-transition-group'
+import { StyleProvider } from '@ant-design/cssinjs'
 
 import PageArticle from './article/index'
 import Main from './topics/index'
@@ -16,18 +17,20 @@ import { type AppStore, RootContext } from '@/store'
 function App({ store }: { store: AppStore }) {
     const location = useLocation()
     return (
-        <RootContext.Provider value={store}>
-            <div className="main flex flex-col">
-                <TransitionGroup appear>
-                    <CSSTransition classNames="example" in={false} key={location.key} timeout={{ appear: 300, enter: 300, exit: 300 }}>
-                        <Routes>
-                            <Route path="/" element={<Main />} />
-                            <Route path="/article/:id" element={<PageArticle />} />
-                        </Routes>
-                    </CSSTransition>
-                </TransitionGroup>
-            </div>
-        </RootContext.Provider>
+        <StyleProvider hashPriority="high">
+            <RootContext.Provider value={store}>
+                <div className="main flex flex-col">
+                    <TransitionGroup appear>
+                        <CSSTransition classNames="example" in={false} key={location.key} timeout={{ appear: 300, enter: 300, exit: 300 }}>
+                            <Routes>
+                                <Route path="/" element={<Main />} />
+                                <Route path="/article/:id" element={<PageArticle />} />
+                            </Routes>
+                        </CSSTransition>
+                    </TransitionGroup>
+                </div>
+            </RootContext.Provider>
+        </StyleProvider>
     )
 }
 export default App
