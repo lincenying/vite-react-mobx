ac9fdbcae2b8d1830214c5094b06b81b17d75667
林岑影 <lincenying@qq.com>
2024-02-04 15:57:42 +0800

    chore: update deps

--- vite.config.build.ts
+++ vite.config.build.ts
@@ -16,6 +16,12 @@ const config: { server: ServerOptions; build: BuildOptions } = {
                 rewrite: (path: string) => path.replace(/^\/api/, '/api'),
             },
         },
+        warmup: {
+            clientFiles: [
+                './src/main.tsx',
+                './src/pages/**/*.tsx',
+            ],
+        },
     },
     build: {
         target: 'es2018',
